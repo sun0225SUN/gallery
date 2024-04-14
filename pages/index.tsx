d8309111eda81b6e3707e7dc7e0faddf6ad3bce6
Guoqi Sun <sun412926@gmail.com>
2024-04-14 21:15:16 +0800

    feat: fix style layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,13 +97,13 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
                 style={{ transform: "translate3d(0, 0, 0)" }}
                 placeholder="blur"
                 blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720,h_480/${public_id}.${format}`}
+                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale/${public_id}.${format}`}
                 width={720}
                 height={480}
                 sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw"
+                (max-width: 1280px) 50vw,
+                (max-width: 1536px) 33vw,
+                25vw"
               />
             </Link>
           ))}
